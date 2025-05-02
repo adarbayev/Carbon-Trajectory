2724373fb0d420b673e6317f8939fbebf308adab
adarbayev <aa.darbayev@gmail.com>
2025-05-02 14:32:37 +0100

    Update thinkhazard.js

--- thinkhazard.js
+++ thinkhazard.js
@@ -1,7 +1,7 @@
 export async function fetchHazards(lat, lon) {
-  const prox = 'https://corsproxy.io/?';
   const url  = `https://www.thinkhazard.org/en/report/bycoordinates?lat=${lat}&lon=${lon}`;
-  const res  = await fetch(prox + encodeURIComponent(url));
+  const prox = 'https://corsproxy.io/?';   // NOTE: no encoding!
+  const res  = await fetch(prox + url);     // just concatenate, no encodeURIComponent
   if (!res.ok) throw new Error('proxy failed');
   return res.json().then(j => j.hazards);
 } 
